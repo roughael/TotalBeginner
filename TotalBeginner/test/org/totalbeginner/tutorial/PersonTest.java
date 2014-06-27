1df0ce41a35c45eb77641ae71a1844d740e88042
roughael <rafaelallozalahoz@gmail.com>
2014-06-28 00:01:37 +0200

    Versión inicial

---
+++ TotalBeginner/test/org/totalbeginner/tutorial/PersonTest.java
@@ -0,0 +1,33 @@
+package org.totalbeginner.tutorial;
+
+import junit.framework.TestCase;
+
+public class PersonTest extends TestCase {
+
+	public void testPerson() {
+		Person p1 = new Person();
+		assertEquals("unknown name", p1.getName());
+		assertEquals(3, p1.getMaximumBooks());
+	}
+
+	public void testSetName() {
+		Person p2 = new Person();
+		p2.setName("Fred");
+		assertEquals("Fred", p2.getName());
+	}
+
+	public void testSetMaximumBooks() {
+		Person p3 = new Person();
+		p3.setMaximumBooks(10);
+		assertEquals(10, p3.getMaximumBooks());
+	}
+	
+	public void testToString(){
+		Person p4 = new Person();
+		p4.setName("Fred Flinstone");
+		p4.setMaximumBooks(7);
+		String testString = "Fred Flinstone (7 books)";
+		assertEquals(testString, p4.toString());
+	}
+
+}
